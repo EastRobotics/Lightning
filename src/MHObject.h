201b1d58f36487c00b2f61284dd7122ecf148aea
Michael Hulet <bestraysfan@me.com>
2015-03-10 20:47:48 -0400

    Added copyright and licensing to top of framework files

--- src/MHObject.h
+++ src/MHObject.h
@@ -1,3 +1,23 @@
+/*Copyright (c) 2015 Frightening Lightning. All rights reserved.
+
+ Developed by: Michael Hulet
+               https://github.com/raysarebest
+
+
+See the project LICENSE file (https://github.com/EastRobotics/Lightning/blob/master/LICENSE)
+for the full license of this file.
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+WITH THE SOFTWARE.*/
+
 //Make conditional #including of MHObject possible
 #ifndef MHOBJECT
 #define MHOBJECT
