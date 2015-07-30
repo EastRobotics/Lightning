22c9623ee8ff3b271a2fa37610bbb66e592938f2
Michael Hulet <bestraysfan@me.com>
2015-07-30 15:29:09 -0500

    Converted global header to new naming, and applied unsupported errors

--- src/Lightning.h
+++ src/Lightning.h
@@ -19,21 +19,34 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 WITH THE SOFTWARE.*/
 
-//Silence any unreferenced function/variable warnings
+//Prevent programmers from trying to use this on unsupported platforms
+#if defined(__LIGHTNING_UNSUPPORTED__) && (!defined(VEX) || !defined(VEX2) || !defined(VexIQ))
+#define __LIGHTNING_UNSUPPORTED__
+#ifndef LIGHTNING_UNSUPPORTED_WARNING
+#error "Lightning is only designed to function on VEX platforms"
+#else
+#warning "Lightning is only designed to function on VEX platforms"
+#endif
+#else
+
+//Make sure this file is only #included once
+#pragma once
+//Silence any "Unreference function/variable warnings
 #pragma systemFile
 
 //Make conditional #including of Lightning easily possible
-#ifndef LIGHTNING
-#define LIGHTNING
+#ifndef __LIGHTNING__
+#define __LIGHTNING__
 
-//#include MHObject if it hasn't been done so already
-#ifndef MHOBJECT
-#include "MHObject.h"
+//#include FLObject if it hasn't been done so already
+#ifndef __FLOBJECT_H__
+#include "FLObject.h"
 #endif
 
-//#include MHMotor if it hasn't been done so already
-#ifndef MHMOTOR
-#include "MHMotor.h"
+//#include FLMotor if it hasn't been done so already
+#ifndef __FLMOTOR_H__
+#include "FLMotor.h"
 #endif
 
+#endif
 #endif
